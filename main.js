398a145a9d9cb83cf77c08dd3fef30d5abc473b8
Serg <34738549+javser@users.noreply.github.com>
2019-10-26 10:26:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 try{
-alert(70);p()
+with(document){
+var dlc=function(){
+alert(7);
+removeEventListener("DOMContentLoaded", dlc,false);
+};
+addEventListener("DOMContentLoaded", dlc,false);
+}
 }catch(e){alert(e)}
