3306edd13963349c2652abe217c913b62b6dfd81
Serg <34738549+javser@users.noreply.github.com>
2019-10-26 13:33:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 try{
-
-document.addEventListener("DOMContentLoaded", function(){
-document.body.innerHTML+="<h3>title</h3>"
-});
+with( document ) {
+body.innerHTML+="<h3>title</h3>"
+}
 
 }catch(e){alert(e)}
