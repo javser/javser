4ff9a8962e8b714498d83a9cb52cc547ac87a28c
Serg <34738549+javser@users.noreply.github.com>
2019-11-12 09:00:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 alert(Math.random()*100);
 alert(61);
-let deferredPrompt;
 
-//window.addEventListener('beforeinstallprompt', (e) => {
+var bt = document.createElement('button');
+bt.textContent = 'install';
+bt.onclick = install();
+document.body.appendchild(bt);
+
+let deferredPrompt = null;
+
+window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
-  //e.preventDefault();
+  e.preventDefault();
   // Stash the event so it can be triggered later.
-  //deferredPrompt = e;
-  //alert(7);
-//});
+  deferredPrompt = e;
+});
 
+async function install() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    console.log(deferredPrompt)
+    deferredPrompt.userChoice.then(function(choiceResult){
+
+      if (choiceResult.outcome === 'accepted') {
+      console.log('Your PWA has been installed');
+    } else {
+      console.log('User chose to not install your PWA');
+    }
+
+    deferredPrompt = null;
+
+    });
+
+
+  }
+}
